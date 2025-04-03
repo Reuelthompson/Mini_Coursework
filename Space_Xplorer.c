8421b1df0f72bd00b721b4949300631c4087d78a
Reuel <Reuel2.thompson@live.uwe.ac.uk>
2025-04-03 11:53:10 +0100

    First attempt

--- Space_Xplorer.c
+++ Space_Xplorer.c
@@ -1,7 +1,7 @@
 #include "game.h"
 
 void init(void);    //Initialise player position
-//fill array with 0 and 1
+//fill array with 0 and 1 cccc
 int main(void) {
     printf("Hello, World!\n");
     return 0;
