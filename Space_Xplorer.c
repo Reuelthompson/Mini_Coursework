39cd69245f6680dda7d9b852fd7f1e847a852baf
ReuelThompson <Reuel2.thompson@live.uwe.ac.uk>
2025-04-18 16:56:03 +0100

    Third Push, with instructions and a functional grid. also initialisation of player data

--- Space_Xplorer.c
+++ Space_Xplorer.c
@@ -1,21 +1,30 @@
 #include "game.h"
 FILE *fptr;
 
-void init(World *world);    //Initialise player position and world
+void World_init(World *world);    //Initialise player position and world
 void print_grid(const World *world);
-void print_asteroids(World *world);
-void update(World *world);
+void World_update(World *world);
+
+void player_name(player_data *player);
+void player_data_init(player_data *player);
+void player_data_update(player_data *player);
+void print_player_data(player_data *player);
+void player_move(player_data *player);
+
 void instructions();
 
 int main(void) {
 
-    World world = {};
-    world.asteroids = ASTEROIDS;
-    player_data player_data = {};
-
     instructions();
 
-    init (&world);
+    World world = {};
+    World_init (&world);
+
+    player_data player = {};
+    player_name(&player);
+    player_data_init (&player);
+    print_player_data(&player);
+
     print_grid (&world);
     //printf("Hello, World!\n");
     return 0;
@@ -32,14 +41,62 @@ void instructions() {
     }
     fclose(fptr);// Close the file
 }
-void init(World *world) {                       //function to initialise the world or reset if needed
+void player_name(player_data *player) {
+    printf("Enter First Name: \n");
+    scanf("%s", &player->person.First_name);
+    printf("Enter Initial: \n");
+    scanf(" %c", &player->person.Initial);
+    printf("Enter Surname: \n");
+    scanf("%s", &player->person.Surname);
+}
+void player_data_init(player_data *player) {    //function to initialise the player data
+    player->location.x           = 0;
+    player->location.y           = 9;
+    player->score                = 0;
+    player->fuel               = 100;
+    player->space_junk_collected = 0;
+}
+void print_player_data(player_data *player) {   //function for testing, to see  what data is stored
+    printf("First Name: %s\n", player->person.First_name);
+    printf("Initial: %c\n", player->person.Initial);
+    printf("Surname: %s\n", player->person.Surname);
+    printf("x_coordinate: %d\n", player->location.x);
+    printf("y_coordinate: %d\n", player->location.y);
+    printf("score: %d\n", player->score);
+    printf("fuel: %d\n", player->fuel);
+    printf("space_junk_collected: %d\n", player->space_junk_collected);
+}
+void World_init(World *world) {                  //function to initialise the world or reset if needed
+    world->asteroids = ASTEROIDS;
+    world->space_junk= SPACE_JUNK;
     for (int i = 0; i < WORLD_SIZE_X; i++) {
         for (int j = 0; j < WORLD_SIZE_Y; j++) {
             world->grid[i][j] = 'O';
         }
     }
 }
-void print_grid(const World *world) {
+/*void update(World *world, player_data *player) {
+    char direction;
+    printf("What is your next move?\n");
+    scanf("%c", &direction);
+    switch (direction) {
+        case 'w':
+            player_move(player);
+        break;
+        case 'a':
+            player_move(player);
+        break;
+        case 's':
+            player_move(player);
+        break;
+        case 'd':
+            player_move(player);
+        break;
+        default:
+            break;
+    }
+}*/
+void print_grid(const World *world) {           //function to print out the world
     for (int i = 0; i < WORLD_SIZE_X; i++) {
         for (int j = 0; j < WORLD_SIZE_Y; j++) {
             printf("    %c", world->grid[i][j]);
@@ -47,5 +104,3 @@ void print_grid(const World *world) {
         printf ("\n");
     }
 }
-void print_asteroids(World *world) {}
-void update(World *world) {}
--- game.h
+++ game.h
@@ -4,28 +4,38 @@
 
 #ifndef GAME_H
 #define GAME_H
-//------------------------------------------------
+//-------------------Global Libraries------------------------------
 #include <stdio.h>
 #include <stdlib.h>
-//------------------------------------------------
+//-------------------Global defines--------------------------------
 #define WORLD_SIZE_X 18
 #define WORLD_SIZE_Y 18
 #define ASTEROIDS    20
-//------------------------------------------------
+#define SPACE_JUNK   10
+#define NAME_LENGTH  20
+//-------------------Global structures-----------------------------
 typedef struct{
   int x;
   int y;
 }coordinates;
 
+typedef struct {
+    char First_name[NAME_LENGTH];   //initialise variables inside a structure
+    char Initial;
+    char Surname[NAME_LENGTH];
+}person_name;                       //Using Typdef to simplify Syntax further on
+
 typedef struct{
   coordinates location;
+  person_name person;
   int fuel;
-  int space_junk;
+  int space_junk_collected;
   int score;
  }player_data;
 
 typedef struct {
     int asteroids;
+    int space_junk;
     char grid[WORLD_SIZE_X][WORLD_SIZE_Y];
 }World;
 
